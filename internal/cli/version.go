bce7426227b77fedd3399386c0b3312012aa94aa
Juan Olvera <juan@jolvera.dev>
2026-03-21 17:14:26 -0500

    init

---
+++ internal/cli/version.go
@@ -0,0 +1,18 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func newVersionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print the goetry version",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Fprintf(cmd.OutOrStdout(), "goetry %s\n", Version)
+			return nil
+		},
+	}
+}
