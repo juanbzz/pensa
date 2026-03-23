12c8f74ab06ff859f8b704bab4c42b4bd3958026
Juan Olvera <juan@jolvera.dev>
2026-03-23 11:57:38 -0500

    chore: colored help output

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +16,38 @@ func newRootCmd() *cobra.Command {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 	}
+
+	// Color the help template.
+	heading := color.New(color.Bold, color.FgBlue).SprintFunc()
+	cmdName := color.New(color.FgGreen).SprintFunc()
+	flagStyle := color.New(color.FgYellow).SprintFunc()
+	cobra.AddTemplateFunc("heading", heading)
+	cobra.AddTemplateFunc("cmdName", cmdName)
+	cobra.AddTemplateFunc("flagStyle", flagStyle)
+	cmd.SetUsageTemplate(`{{heading "Usage:"}}
+  {{.UseLine}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+{{heading "Aliases:"}}
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+{{heading "Examples:"}}
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+{{heading "Available Commands:"}}{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{cmdName (rpad .Name .NamePadding)}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+{{heading "Flags:"}}
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+{{heading "Global Flags:"}}
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+{{heading "Additional help topics:"}}{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{flagStyle .CommandPath}} [command] --help" for more information about a command.{{end}}
+`)
 
 	cmd.AddCommand(newVersionCmd())
 	cmd.AddCommand(newNewCmd())
