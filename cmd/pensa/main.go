b5fc860a1979281d2429ab2ed5951f65731de8cc
Juan Olvera <juan@jolvera.dev>
2026-03-24 19:51:42 -0500

    fix: gitignore excluding cmd/pensa dir

---
+++ cmd/pensa/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/juanbzz/pensa/internal/cli"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	return cli.Execute()
+}
